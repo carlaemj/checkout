d17e035be4e7a43ea9086a8851de98801ae2bb9b
Carla Martínez <carla.martinez@cabify.com>
2019-03-20 14:34:19 +0100

    Added tests for discounts module.

---
+++ test/discount_test.exs
@@ -0,0 +1,34 @@
+
+defmodule DiscountTest do
+
+  use ExUnit.Case
+
+  test "3 x 2 and remains" do
+
+    discount_func = Discount.get("MANY_FOR_MANY", 3, 2)
+    assert(discount_func.(4, 15) == 45.00)
+
+  end
+
+  test "3 x 2 no remains" do
+
+    discount_func = Discount.get("MANY_FOR_MANY", 3, 2)
+    assert(discount_func.(6, 15) == 60.00)
+
+  end
+
+  test "30% discount 2 or more" do
+
+    discount_func = Discount.get("PRICE_REDUCTION", 7.0, 2)
+    assert(discount_func.(10, 12.0) == 70.00)
+
+  end
+
+  test "30% discount 2 or more only 1" do
+
+    discount_func = Discount.get("PRICE_REDUCTION", 7.0, 2)
+    assert(discount_func.(1, 12.0) == 12.00)
+
+  end
+
+end
